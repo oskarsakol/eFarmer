48597ebad74a3e3d7eeea6ba8e0e177d966f5b3d
Oskar Sąkol <sakoloskar@gmail.com>
2020-01-20 23:54:06 +0100

    Basic fixes for apps, models and urls

--- efarmer/advertisements/serializers.py
+++ efarmer/advertisements/serializers.py
@@ -1,26 +1,11 @@
-import re
-
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from .models import Advertisement
-from django.utils.translation import gettext_lazy as _
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
-
-    def validate_phone(self, value):
-        """
-        Validates phone number of an organization using Regex
-        :param value: phone number
-        :return: value or raises ValidationError
-        """
-        pattern = re.compile(r'(^[+0-9]{1,3})*([0-9]{8,15}$)', re.IGNORECASE)
-        value = value.replace(" ", "")
-        if pattern.match(value) is None:
-            raise ValidationError(_('Please insert correct phone number.'))
-        return value
+    auction_from = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 
     class Meta:
         model = Advertisement
         fields = '__all__'
-        read_only_fields = ('name',)
+        read_only_fields = ('auction_from',)
--- efarmer/users/serializers.py
+++ efarmer/users/serializers.py
@@ -1,13 +1,16 @@
+import re
+
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name',)
-        read_only_fields = ('username', )
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'phone', 'address', 'city')
+        read_only_fields = ('username',)
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
@@ -18,7 +21,20 @@ class CreateUserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
+    def validate_phone(self, value):
+        """
+        Validates phone number of an organization using Regex
+        :param value: phone number
+        :return: value or raises ValidationError
+        """
+        pattern = re.compile(r'(^[+0-9]{1,3})*([0-9]{8,15}$)', re.IGNORECASE)
+        value = value.replace(" ", "")
+        if pattern.match(value) is None:
+            raise ValidationError(_('Please insert correct phone number.'))
+        return value
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email',)
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'phone',
+                  'address', 'city')
         extra_kwargs = {'password': {'write_only': True}}
